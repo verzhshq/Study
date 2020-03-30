f10287aa0883d8b4a2e3db1bffd2f1579e83b38b
Zhang <375977383@qq.com>
2020-03-30 16:04:41 +0800

    several problems

--- LeetCode/src/Solution/Solution.java
+++ LeetCode/src/Solution/Solution.java
@@ -1,15 +1,52 @@
 package Solution;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println();
+        System.out.println(s.defangIPaddr("1.1.1.1"));
+    }
+
+
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            if (c == '.')
+                sb.append("[.]");
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+
+
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            int[] left = count(rating, 0, i, rating[i]);
+            int[] right = count(rating, i, n, rating[i]);
+            result += left[0] * right[1] + left[1] * right[0];
+        }
+        return result;
+    }
+
+    private int[] count(int[] rating, int from, int to, int key) {
+        int[] results = new int[2];
+        for (int i = from; i < to; ++i) {
+            if (rating[i] < key)
+                ++results[0];
+            else if (rating[i] > key)
+                ++results[1];
+        }
+        return results;
+    }
+
+
+    public int lastRemaining(int n, int m) {
+        return n == 1 ? 0 : (lastRemaining(n - 1, m) + m) % n;
     }
 
     public int[] createTargetArray(int[] nums, int[] index) {
